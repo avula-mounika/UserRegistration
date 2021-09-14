2ff80734988cb45c19bbd34bb8d8021731f4769e
avula-mounika <goudmounika13@gmail.com>
2021-09-14 21:31:05 +0530

    last name validation

--- UserRegex.java
+++ UserRegex.java
@@ -7,10 +7,10 @@ public class UserRegex {
 		
 		Scanner Scan = new Scanner(System.in);
 		
-		String firstName="^[A-Z][a-zA-Z]{2,20}";
+		String lastName="^[A-Z][a-zA-Z]{2,20}";
 		System.out.println("Enter a name with minimum three alphabets");
 		 String name = Scan.next();
-		System.out.println("Is the input name valid ? : "+name.matches(firstName));
+		System.out.println("Is the input name valid ? : "+name.matches(lastName));
 				        
 	}
 
